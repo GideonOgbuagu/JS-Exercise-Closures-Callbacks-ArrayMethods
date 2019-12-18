b99587eb25a87274a7b843c6fb79b8b667b8e778
Gideon <reachgiddy@gmail.com>
2019-12-18 00:43:33 -0500

    passed test for firstNameAllCaps() & getRunnersByTShirtSize()

--- index.js
+++ index.js
@@ -230,8 +230,12 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+     let mapped = runners.map((element, index, array) => {
+      return element.first_name.toUpperCase();
+    })
+    return mapped;
+    
 }
 
 /**
@@ -247,8 +251,11 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  let filtered = runners.filter((element, index, array) => {
+    return tShirtSize === element.shirt_size;
+  })
+  return filtered;
 }
 
 /**
